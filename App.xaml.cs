3305cf9028ea1305451bcf567eff1098874df09a
Anto <antonella.cortes@udla.edu.ec>
2023-02-01 08:59:47 -0500

    Crear repo para los juegos.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,16 @@
-﻿namespace AntonellaCortes_PROGRESOFINAL;
+﻿using AntonellaCortes_PROGRESOFINAL.Data;
+using AntonellaCortes_PROGRESOFINAL.ACViews;
+
+namespace AntonellaCortes_PROGRESOFINAL;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public static ACGameDataBase GameRepo { get; set; }
+    public App(ACGameDataBase repo)
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+        GameRepo = repo;
+    }
 }
