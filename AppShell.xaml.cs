4505aa4d516aa625250c37b18a046dd010237fd9
Anto <antonella.cortes@udla.edu.ec>
2023-02-01 09:02:23 -0500

    Conectar el shell para que inicialice la app con la pantalla de generar recomendación.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace AntonellaCortes_PROGRESOFINAL;
+﻿using AntonellaCortes_PROGRESOFINAL.ACViews;
+
+namespace AntonellaCortes_PROGRESOFINAL;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute(nameof(ACGameDetails), typeof(ACGameDetails));
+        Routing.RegisterRoute(nameof(ACGenerateGame), typeof(ACGenerateGame));
+    }
 }
