7cadc35083d7a9d8af447bf52d4e9fcb77f83d47
Anto <antonella.cortes@udla.edu.ec>
2023-02-01 08:42:26 -0500

    Creación de la base de datos.

---
+++ ACData/ACGameDataBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using AntonellaCortes_PROGRESOFINAL.Models;
+
+namespace AntonellaCortes_PROGRESOFINAL.Data
+{
+    public class ACGameDataBase
+    {
+        string _dbPath;
+        private SQLiteConnection conn;
+
+        public ACGameDataBase(string DatabasePath)
+        {
+            _dbPath = DatabasePath;
+        }
+
+        private void Init()
+        {
+            if (conn != null)
+                return;
+
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<ACGame>();
+        }
+
+        public int AddNewGame(ACGame game)
+        {
+            Init();
+            int result = conn.Insert(game);
+            return result;
+        }
+
+        public int UpadateGame(ACGame game)
+        {
+            Init();
+            int result = conn.Update(game);
+            return result;
+        }
+
+        public int DeleteGame(ACGame game)
+        {
+            Init();
+            int result = conn.Delete(game);
+            return result;
+        }
+
+        public List<ACGame> GetAllGames()
+        {
+            Init();
+            List<ACGame> games = conn.Table<ACGame>().ToList();
+            return games;
+        }
+
+        public ACGame GetGame(int id)
+        {
+            ACGame game1 = new ACGame(); //ham
+            Init();
+            List<ACGame> games = conn.Table<ACGame>().ToList();
+            foreach (ACGame game2 in games) //hame
+            {
+                if (game2.ID == id)
+                    game1 = game2;
+            }
+            return game1;
+        }
+    }
+}
