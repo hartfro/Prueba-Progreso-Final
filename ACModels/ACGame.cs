57c96d2d2f9df74e7e6827ee1e703fc7e5a9e8d2
Anto <antonella.cortes@udla.edu.ec>
2023-02-01 08:46:12 -0500

    Crear clase en Models con las propiedades de juego.

---
+++ ACModels/ACGame.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableAttribute = SQLite.TableAttribute;
+using MaxLengthAttribute = SQLite.MaxLengthAttribute;
+
+namespace AntonellaCortes_PROGRESOFINAL.Models
+{
+    [Table("game")]
+    public class ACGame // registro de recomendaciones de juego
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string ACUserName { get; set; }
+        public string title { get; set; }
+        //public string thumbnail { get; set; }
+        public string short_description { get; set; }
+        //public string game_url { get; set; }
+        public string genre { get; set; }
+        public string platform { get; set; }
+        public string publisher { get; set; }
+        public string developer { get; set; }
+        public string release_date { get; set; }
+        //public string freetogame_profile_url { get; set; }
+    }
+}
