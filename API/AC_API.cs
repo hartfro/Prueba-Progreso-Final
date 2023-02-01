439e1cc83a2a3bed3a40c5512954e529bf2e7aee
Anto <antonella.cortes@udla.edu.ec>
2023-02-01 08:32:02 -0500

    Crear carpeta API y clase correspondiente (traducción de json a C#) para manejar la API seleccionada.

---
+++ API/AC_API.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AntonellaCortes_PROGRESOFINAL.API
+{
+    public class Rootobject
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string thumbnail { get; set; }
+        public string short_description { get; set; }
+        public string game_url { get; set; }
+        public string genre { get; set; }
+        public string platform { get; set; }
+        public string publisher { get; set; }
+        public string developer { get; set; }
+        public string release_date { get; set; }
+        public string freetogame_profile_url { get; set; }
+    }
+}
